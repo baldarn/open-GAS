2409f6a96323316e9cc22dda6d31484a01057c53
lorenzo farnararo <lorenzo.farnararo@suse.com>
2024-08-12 22:59:57 +0200

    allow browsers modern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
 
-  # allow_browser versions: :modern # wait rails 7.2
+  allow_browser versions: :modern
 end
