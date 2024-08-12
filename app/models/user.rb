f733fdc6440f0c6cf5f80bb3e691488072c075e2
lorenzo farnararo <lorenzo.farnararo@suse.com>
2024-08-13 00:14:20 +0200

    deploy and timeout for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable
+         :confirmable, :lockable, :timeoutable
 
   has_one_attached :picture
 
