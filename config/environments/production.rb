108259b6cd024e4176a1bcf2f7e896664693a70e
lorenzo farnararo <lorenzo.farnararo@suse.com>
2024-08-17 22:09:13 +0200

    deploy fix for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,8 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: ENV.fetch('APP_HOST', 'localhost') }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
