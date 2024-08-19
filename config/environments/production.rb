0a6bd3c1f3bf5abb73c633c84566f3b90c9117f5
lorenzo farnararo <lorenzo.farnararo@suse.com>
2024-08-19 21:58:12 +0200

    better ui and functions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,8 +38,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :s3
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
