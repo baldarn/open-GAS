1ccdf003a72ecbd38378d45b2a5a00ceffe92932
lorenzo farnararo <lorenzo.farnararo@suse.com>
2024-09-12 20:20:34 +0200

    add code coverage

--- Gemfile
+++ Gemfile
@@ -83,4 +83,5 @@ end
 group :test do
   gem 'capybara'
   gem 'selenium-webdriver'
+  gem 'simplecov', require: false
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
